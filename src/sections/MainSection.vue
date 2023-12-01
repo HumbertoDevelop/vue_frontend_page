dd477ab957a71b8c2fc430541df1efd4cb0cf705
Humberto García <humbertodev14@gmail.com>
2023-12-01 17:44:18 -0400

    first commit

--- src/sections/MainSection.vue
+++ src/sections/MainSection.vue
@@ -1,13 +1,13 @@
 <template>
     <!-- Main section start -->
 	<section class="h-screen px-12 lg:px-32 py-32 mb-8">
-		<div class="md:w-72 lg:w-56 xl:w-full h-full flex flex-col md:justify-center">
+		<div class="md:w-72 xl:w-2/4  h-full flex flex-col md:justify-center">
 			<!-- Main text start -->
-			<h1 class="uppercase text-2xl sm:text-5xl md:text-3xl xl:text-7xl mb-4 font-semibold break-words">
+			<h1 class="uppercase text-2xl sm:text-5xl md:text-3xl xl:text-6xl mb-4 font-semibold break-words">
 				CITY KOOL 
 				DORAL DISTRIBUTTORS
 			</h1>
-			<p class="capitalize w-52 xl:w-1/2 text-stone-500">
+			<p class="capitalize w-52 md:w-72 xl:w-1/2 text-stone-500">
 				Our team of experts is dedicated to providing customized and efficient solutions for temperature and air quality management in any type of space.
 			</p>
 			<!-- Main text end -->
@@ -34,7 +34,7 @@
 		</div>
 
 		<!-- Photographer image start -->
-		<div class="absolute bottom-0 lg:top-0 right-0 pt-14 overflow-hidden h-2/5 sm:h-1/2 md:h-3/5 xl:h-4/6 -z-50 py-10 mt-24">
+		<div class="absolute bottom-0 lg:top-0 right-20 pt-14 overflow-hidden h-2/5 sm:h-1/2 md:h-3/5 xl:h-4/6 -z-50 py-10 mt-24">
 			<img src="@/assets/business/business2.jpeg" alt="img" class="h-full">
 		</div>
 
